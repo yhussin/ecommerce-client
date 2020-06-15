5e60076ad06f58bc5b096b90abd79cae79613b59
yhussin <yhussin@gmail.com>
2020-06-14 19:18:55 -0700

    Homepage styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-      <div>
-          <h1>Welcome to Ecommerce Home</h1>
+    <>
+
+      <h1 className="center-block text-center">Welcome to Unique Wall Art</h1>
+
+    <div className="row">
+    <div className="col-4 px-md-5">
+        <img src="https://picsum.photos/id/1015/400" alt="main" className="img-thumbnail"></img>
       </div>
+      <div className="col-4 text-center px-md-5">
+        <p>Welcome to Unique Wall Art. We offer the finest prints of nature images, which can be printed according to your needs. These high-quality photos will be a great addition to any space.</p>
+        <p>We also offer wholesale pricing for retailers.</p>
+
+      </div>
+      <div className="col-4">
+        <img src="https://picsum.photos/id/1016/400" alt="main" className="img-thumbnail"></img>
+      </div>
+    </div>
+    </>
   )
 }
 
